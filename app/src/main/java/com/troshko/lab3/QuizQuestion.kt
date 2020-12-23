90909ce231b9e1d452d640232c71008d6b0d34d1
ilyatroshko <troshko.00@mail.ru>
2020-12-23 16:03:22 +0300

    input

---
+++ app/src/main/java/com/troshko/lab3/QuizQuestion.kt
@@ -0,0 +1,10 @@
+package com.troshko.lab3
+
+data class QuizQuestion(
+    val first: String? = null,
+    val second: String? = null,
+    val third: String? = null,
+    val fourth: String? = null,
+    val right: String? = null,
+    val question: String? = null
+)
---
+++ app/src/main/java/com/troshko/lab3/User.kt
@@ -0,0 +1,7 @@
+package com.troshko.lab3
+
+data class User(
+    var position: Int? = null,
+    var name: String? = null,
+    var score: Int? = null
+)
